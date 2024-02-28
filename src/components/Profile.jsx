16a7b0e7f03e468fbf492e6a0376ca0948448b7f
Alejandra Galvan <alejandra.glv.gg@gmail.com>
2024-02-28 14:06:43 -0600

    algo paso

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigate from "./NavigationBar";
+
+const Profile = () => {
+  return (
+    <div>
+        <Navigate/>
+        Profile
+    </div>
+  )
+}
+
+export default Profile
